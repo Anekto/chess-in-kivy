8c1811c9218435310382798f3035cae59681d80c
Anekto <53334178+Anekto@users.noreply.github.com>
2024-02-02 18:56:09 +0100

    Update Schach.py
    Updated some german sentences to english

--- Schach.py
+++ Schach.py
@@ -8,6 +8,7 @@ white_Pieces = ["wT ", "wL ", "wS ", "wK ", "wKK", "wS ", "wL ", "wT ", "wB ", "
 black_Pieces = ["sT ", "sL ", "sS ", "sK ", "sKK", "sS ", "sL ", "sT ", "sB ", "___"]
 
 def main():
+    # For debugging commented out change accordingly 
     #game_Start = input("Welcome to the chess app. Do you want to play a round ? Type 'y' or 'n' !")
 
     game_Start = "y"
@@ -22,7 +23,7 @@ def main():
     else:
         print("The game will close now.")
         start = False
-    print("\n Weiß beginnt mit dem ersten Zug!")
+    print("\n White makes the first move")
 
     while start:
         if start_game == 1:
@@ -31,8 +32,8 @@ def main():
         else:
             board_layout.layout(start, state)
 
-        pos1 = input("\n Bitte gebe die Koordinaten für die zu bewegende Figur an z.B. '1-c'")
-        pos2 = input("Bitte gebe die Koordinaten wohin die Figur ziehen soll an z.B. '3-d'")
+        pos1 = input("\n Please enter coordinates to move a piece e.g. 'FROM' '1-c'")
+        pos2 = input("Please enter coordinates to move a piece to a position 'TO' e.g. '3-d'")
         try:
             pos1 = [int(pos1[0]), play_Pieces.char_to_int(pos1[2]) - 1]
             pos2 = [int(pos2[0]), play_Pieces.char_to_int(pos2[2]) - 1]
@@ -44,19 +45,19 @@ def main():
         if player == 1:
             if state[pos1[0]][pos1[1]] in white_Pieces[0:9]:
                 state = piece_Move_2.make_move(pos1, pos2, state, turn, player)
-                print("Weiß macht seinen Zug")
+                print("White moves.")
                 player = 2
             else:
                 player = 1
-                print("Falsche Farbe bewegt, Spieler 1 bitte noch einmal!")
+                print("Tried to move wrong color, try again")
         if player == 2:
             if state[pos1[0]][pos1[1]] in black_Pieces[0:9]:
                 state = piece_Move_2.make_move(pos1, pos2, state, turn, player)
-                print("Schwarz macht seinen Zug")
+                print("Black moves.")
                 player = 1
             else:
                 player = 2
-                print("Falsche Farbe bewegt, Spieler 2 bitte noch einmal!")
+                print("Tried to move wrong color, try again")
 
 
         king_Kill = schachpruefung.schach(state)
